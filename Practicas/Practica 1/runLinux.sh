6e94fc61456cee6f845332eddf84e1b1608768fc
Carlos Cruz Rangel <79823316+CarlosCruzRangel@users.noreply.github.com>
2023-02-20 23:41:41 -0600

    Add RUN

---
+++ Practicas/Practica 1/runLinux.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo " _______  ___   _______  __    _  __   __  _______  __    _  ___   ______   _______ ";
+echo "|  _    ||   | |       ||  |  | ||  | |  ||       ||  |  | ||   | |      | |       |";
+echo "| |_|   ||   | |    ___||   |_| ||  |_|  ||    ___||   |_| ||   | |  _    ||   _   |";
+echo "|       ||   | |   |___ |       ||       ||   |___ |       ||   | | | |   ||  | |  |";
+echo "|  _   | |   | |    ___||  _    ||       ||    ___||  _    ||   | | |_|   ||  |_|  |";
+echo "| |_|   ||   | |   |___ | | |   | |     | |   |___ | | |   ||   | |       ||       |";
+echo "|_______||___| |_______||_|  |__|  |___|  |_______||_|  |__||___| |______| |_______|";
+
+
+
+
+
+
+echo "BIENVENIDO $USER A LA PRACTICA UNO "
+    date
+
+echo
+echo "EMPIEZA LA COMPILACION DEL PROYECTO"
+echo
+    cd ../src
+    javac Practica1.java
+
+echo "TERMINA LA COMPILACION DEL PROYECTO"
+echo
+echo "Presione una tecla para continuar . . ."
+read 
+echo
+\
+echo "EMPIEZA LA EJECUCION DEL PROYECTO"
+    java Main
+
---
+++ Practicas/Practica 1/runWindows.bat
@@ -0,0 +1,28 @@
+@echo off 
+echo
+echo " _______  ___   _______  __    _  __   __  _______  __    _  ___   ______   _______ ";
+echo "|  _    ||   | |       ||  |  | ||  | |  ||       ||  |  | ||   | |      | |       |";
+echo "| |_|   ||   | |    ___||   |_| ||  |_|  ||    ___||   |_| ||   | |  _    ||   _   |";
+echo "|       ||   | |   |___ |       ||       ||   |___ |       ||   | | | |   ||  | |  |";
+echo "|  _   | |   | |    ___||  _    ||       ||    ___||  _    ||   | | |_|   ||  |_|  |";
+echo "| |_|   ||   | |   |___ | | |   | |     | |   |___ | | |   ||   | |       ||       |";
+echo "|_______||___| |_______||_|  |__|  |___|  |_______||_|  |__||___| |______| |_______|";
+echo.
+echo.
+echo BIENVENIDO %USERNAME%
+echo.%TIME% %DATE%
+echo.
+echo.
+echo EMPIEZA LA COMPILACION DE LA PRACTICA
+echo.
+    cd src
+    javac Practica1.java
+echo.
+echo TERMINO LA COMPILACION DE LA PRACTICA
+pause 
+echo EMPIEZA LA EJECUCION DE LA PRACTICA
+
+java Practica1
+
+pause
+pause exit
