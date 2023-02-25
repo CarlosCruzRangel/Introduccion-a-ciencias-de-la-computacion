ac14eb2de6e3cc3bdfc6bfe48a65634e96c2def5
toprakshakur <toprakshakur@ciencias.unam.mx>
2023-02-25 12:21:55 -0600

    Se resolvio la actividad 4

---
+++ Practicas/Practica 1/src/actividad4.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/*
+ * Programa que imprime todos los numeros pares entre dos numeros
+ * que se le piden al usuario.Validara si el primer numeri que se 
+ * escribe es menor o igual a segundo numero,se imprimen los valores
+ * y se acaba el programa,caso contrario manda un mensaje al usuario
+ * y le dejara nuevamente volver a intentar.
+ */
+public class actividad4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int primerNumero, segundoNumero;
+
+        do {
+            System.out.print("Escribe el primer numero: ");
+            primerNumero = sc.nextInt();
+            System.out.print("Escribe el segundo numero: ");
+            segundoNumero = sc.nextInt();
+
+            if (primerNumero > segundoNumero) {
+                System.out.println("El primer valor es más grande que el segundo, intenta de nuevo.");
+            }
+        } while (primerNumero > segundoNumero);
+
+        System.out.print("Los primeros numeros pares son: ");
+        for (int i = primerNumero; i <= segundoNumero; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println("\nSe acabo el programa.");
+    }
+}
