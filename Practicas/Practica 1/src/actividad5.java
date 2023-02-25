b91c8f185071463842401a6a3b7b673a642d4be0
toprakshakur <toprakshakur@ciencias.unam.mx>
2023-02-25 13:50:31 -0600

    Actividad 5 resuelta

---
+++ Practicas/Practica 1/src/actividad5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; 
+public class actividad5 {
+    
+    /*
+     * Programa que imprime una escalera de asteriscos y una escalera
+     * invertida de asteriscos.El programa pedira un numero al usuario 
+     * el cual tomara como altura de la escalera
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la altura de la piramide: ");
+        int altura = sc.nextInt();
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n ");
+
+        for (int i = altura; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
