f2c53aa66fe4c34ecf5aec77cf7c5bf52a75b69f
Carlos Cruz Rangel <79823316+CarlosCruzRangel@users.noreply.github.com>
2023-03-31 16:59:29 -0600

    Update: runWindows.bat

--- Practicas/Practica2/runWindows.bat
+++ Practicas/Practica2/runWindows.bat
@@ -23,7 +23,7 @@ echo.
 echo BIENVENIDO %USERNAME%
 echo.%TIME% %DATE%
 echo.
-echo.
+echo. 
 echo EMPIEZA LA COMPILACION DE LA PRACTICA
 echo.
     cd src
@@ -40,23 +40,20 @@ echo EMPIEZA LA EJECUCION DE LA PRACTICA
     echo ------------------------------------------------------------------------------
     echo Este es un menu interectivo ingresa el numero de la opcion que te interese. 
    
-    echo  1     Ejecutar Actividad 1
-    echo  2     Ejecutar Actividad 2
-    echo  3     Ejecutar Actividad 3
-    echo  4     Ejecutar Actividad 4   
-    echo  5     Ejecutar Actividad 5  
-    echo  6     Salir 
+    echo  Opcion 1     Ejecutar Actividad dos
+    echo  Opcion 2     Ejecutar Actividad tres "(Persona.java)"
+    echo  Opcion 3     Ejecutar Actividad cuatro "(Raices.java)"
+    echo  Opcion 4     Ejecutar Actividad cinco "(Password.java)"    
+    echo  Opcion 5     Salir 
 
-    SET /p var= ^> Seleccione una opcion [1-10]:
+    SET /p var= ^> Seleccione una opcion [1-5]:
 
     if "%var%"=="0" goto inicio
     if "%var%"=="1" goto op1
     if "%var%"=="2" goto op2
     if "%var%"=="3" goto op3
     if "%var%"=="4" goto op4
-    if "%var%"=="5" goto op5
-    
-    if "%var%"=="6" goto salir
+    if "%var%"=="5" goto salir
 
     ::Mensaje de error, validación cuando se selecciona una opción fuera de rango
     echo. El numero "%var%" no es una opcion valida, por favor intente de nuevo.
@@ -67,9 +64,10 @@ echo EMPIEZA LA EJECUCION DE LA PRACTICA
 
     :op1
         echo.
-        echo. Has elegido la opcion Actividad 1
+        echo. Has elegido la opcion Actividad 2 
+        echo  Esto es un programa de un juego de Alice y Bob.
         echo.
-        java Actividad1
+        
 
 
         
@@ -79,9 +77,10 @@ echo EMPIEZA LA EJECUCION DE LA PRACTICA
 
     :op2
                echo.
-        echo. Has elegido la opcion Actividad 2
+        echo. Has elegido la opcion Actividad 3
+        echo  Esto es un programa que calcula el IMC de 3 personas.
         echo.
-        java Actividad2
+        java Persona
        
         
         echo.
@@ -90,9 +89,10 @@ echo EMPIEZA LA EJECUCION DE LA PRACTICA
 
     :op3
         echo.
-        echo. Has elegido la opcion Actividad 3
+        echo. Has elegido la opcion Actividad 4
+        echo  Esto es un programa que resulve ecuciones de grado 2
         echo.
-        java Actividad3       
+        java Raices      
 
         
         echo.
@@ -101,19 +101,10 @@ echo EMPIEZA LA EJECUCION DE LA PRACTICA
 
     :op4
         echo.
-        echo. Has elegido la opcion Actividad 4
-        echo.
-        java Actividad4       
-
-        
-        echo.
-        pause
-   goto:inicio
-    :op5
-        echo.
         echo. Has elegido la opcion Actividad 5
+        echo Esto es un programa que genera contraseñas seguras.
         echo.
-        java Actividad5       
+        java Password
 
         
         echo.
