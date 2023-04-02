e16d58ae8aa107874266e39d6a27df22b2ad8453
Carlos Cruz Rangel <79823316+CarlosCruzRangel@users.noreply.github.com>
2023-04-01 18:11:21 -0600

    Fixing: main() and getDiscriminante() in Raices.java

--- Practicas/Practica2/src/Raices.java
+++ Practicas/Practica2/src/Raices.java
@@ -1,24 +1,23 @@
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
  * It calculates the roots of a quadratic equation
  * 
  * @author Carlos Cruz Rangel 312285823
- * @author Toprak Memik Hernandez 
+ * @author Toprak Memik Hernandez
  * 
  * @since 1 abril 2023, UNAM
  * @version v1.0
  */
 
 public class Raices {
-    
+
     // Declaring the variables that will be used in the class.
     private double a;
     private double b;
     private double c;
+    public double discriminante;
 
-    
     // A constructor that receives the three coefficients of the equation.
     public Raices(double a, double b, double c) {
         this.a = a;
@@ -26,19 +25,21 @@ public class Raices {
         this.c = c;
     }
 
-    
     /**
      * This function returns the discriminant of a quadratic equation
      * 
      * @return The discriminant of the quadratic equation.
      */
     public double getDiscriminante() {
-        return Math.pow(b, 2) - 4 * a * c;
+
+        discriminante = Math.pow(b, 2) - 4 * a * c;
+
+        return discriminante;
     }
 
-    
     /**
-     * > This function returns true if the discriminant is greater than or equal to zero, and false
+     * > This function returns true if the discriminant is greater than or equal to
+     * zero, and false
      * otherwise
      * 
      * @return The method is returning a boolean value.
@@ -47,7 +48,6 @@ public class Raices {
         return getDiscriminante() >= 0;
     }
 
-    
     /**
      * It returns true if the discriminant is equal to 0.
      * 
@@ -57,23 +57,23 @@ public class Raices {
         return getDiscriminante() == 0;
     }
 
-    
     /**
-     * This function checks if the equation has real roots, and if it does, it prints them
+     * This function checks if the equation has real roots, and if it does, it
+     * prints them
      */
     public void obtenerRaices() {
         if (tieneRaices()) {
             double x1 = (-b + Math.sqrt(getDiscriminante())) / (2 * a);
             double x2 = (-b - Math.sqrt(getDiscriminante())) / (2 * a);
-            System.out.println("Las raíces son: x1 = " + x1 + ", x2 = " + x2);
+            System.out.println("\033[32m" + "Las raíces son: x1 = " + x1 + ", x2 = " + x2 + "\033[0m");
         } else {
-            System.out.println("La ecuación no tiene solución real.");
+            System.out.println("\033[31m" + "La ecuación no tiene solución real." + "\033[0m");
         }
     }
 
-    
     /**
-     * If the equation has roots, it gets them. If it has a root, it gets it. If it doesn't have a
+     * If the equation has roots, it gets them. If it has a root, it gets it. If it
+     * doesn't have a
      * root, it says so
      */
     public void calcular() {
@@ -81,38 +81,88 @@ public class Raices {
             obtenerRaices();
         } else if (tieneRaiz()) {
             double x = -b / (2 * a);
-            System.out.println("La ecuación tiene una única solución: x = " + x);
+            System.out.println("\033[32m" + "La ecuación tiene una única solución: x = " + x + "\033[0m");
         } else {
-            System.out.println("La ecuación no tiene solución real.");
+            System.out.println("\033[31m" + "La ecuación no tiene solución real." + "\033[0m");
         }
     }
 
-
-
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        double a, b, c;
+        double a = 0, b = 0, c = 0;
+        boolean validInput = false;
 
-        // Prompt the user to input the coefficients of the quadratic equation.
-        while (true) {
+        while (!validInput) {
             try {
-                System.out.print("Ingrese el valor de a: ");
-                a = scanner.nextDouble();
-                System.out.print("Ingrese el valor de b: ");
-                b = scanner.nextDouble();
-                System.out.print("Ingrese el valor de c: ");
-                c = scanner.nextDouble();
-                break;
-            } catch (InputMismatchException e) {
-                System.out.println("Error: entrada inválida. Intente de nuevo.");
-                scanner.nextLine(); // Clear the input buffer.
+                System.out.print("\n \n");
+                System.out.print("\033[35m" + "Ingrese el coeficiente a: " + "\033[0m");
+                a = Double.parseDouble(input.nextLine());
+                System.out.print("\033[35m" + "Ingrese el coeficiente b: " + "\033[0m");
+                b = Double.parseDouble(input.nextLine());
+                System.out.print("\033[35m" + "Ingrese el coeficiente c: " + "\033[0m");
+                c = Double.parseDouble(input.nextLine());
+                validInput = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Debe ingresar valores numéricos válidos. Intente de nuevo.\n");
             }
         }
 
-        Raices raices = new Raices(a, b, c);
+        Raices ecuacion = new Raices(a, b, c);
 
-        raices.calcular();
+        boolean exit = false;
+
+        while (!exit) {
+            
+            // Printing the menu of options.
+            System.out.println("\nMENU DE OPCIONES");
+            System.out.println("--------------------");
+            System.out.println("1 - Obtener discriminante");
+            System.out.println("2 - Verificar si tiene raíces");
+            System.out.println("3 - Verificar si tiene una única raíz");
+            System.out.println("4 - Calcular las raíces (si existen)");
+            System.out.println("5 - Salir");
+            System.out.print("\nIngrese una opción: ");
+
+            String opcion = input.nextLine();
+
+            switch (opcion) {
+
+                // Printing the discriminant of the equation.
+                case "1":
+                    System.out.println("\033[32m" + "\nEl discriminante es: " + ecuacion.getDiscriminante() + "\033[0m");
+                    break;
+
+                // Printing the result of the method tieneRaices()
+                case "2":
+                    System.out.println("\033[32m" + "\n¿Tiene raíces? " + ecuacion.tieneRaices() + "\033[0m");
+                    break;
+
+                // Printing the result of the method tieneRaiz()
+                case "3":
+                    System.out.println("\033[32m" + "\n¿Tiene una única raíz? " + ecuacion.tieneRaiz() + "\033[0m");
+                    break;
+
+                // Calling the method calcular() and printing the result.
+                case "4":
+                    System.out.println();
+                    ecuacion.calcular();
+                    break;
+
+                // Exiting the program.
+                case "5":
+                    exit = true;
+                    break;
+
+                /* A default case for the switch statement. If the user enters a value that is not 1,
+                2, 3, 4 or 5, it will print the message "Opción inválida. Intente de nuevo." and
+                then break the switch statement. */
+                default:
+                    System.out.println("\033[31m" + "\nOpción inválida. Intente de nuevo.\n" + "\033[0m");
+                    break;
+            }
+        }
+
+        input.close();
     }
 }
