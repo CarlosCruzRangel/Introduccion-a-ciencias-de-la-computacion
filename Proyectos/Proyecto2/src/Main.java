384d57abf5c8ba061d2a70430d2165c173252e1c
Carlos Cruz Rangel <79823316+CarlosCruzRangel@users.noreply.github.com>
2023-04-27 18:08:15 -0600

    Update: Main.java & Soresol.java

--- Proyectos/Proyecto2/src/Main.java
+++ Proyectos/Proyecto2/src/Main.java
@@ -1,43 +1,78 @@
 import java.util.Scanner;
 
 public class Main {
-
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-        Solresol solresol = new Solresol();
+        Idioma solresol = new Solresol();
+        
+        System.out.println("¡Bienvenido al programa de Solresol!");
+        System.out.println("====================================");
+        
         boolean salir = false;
-
+        
         while (!salir) {
-            System.out.println("Ingrese una palabra en Solresol (do, re, mi, fa, sol, la, si):");
-            String entrada;
-            try {
-                entrada = scanner.nextLine();
-            } catch (Exception e) {
-                System.out.println("Error al leer la entrada del usuario.");
-                continue;
-            }
-
-            try {
-                if (solresol.esSolresol(entrada)) {
-                    System.out.println("La palabra es válida en Solresol.");
-                } else {
-                    System.out.println("La palabra no es válida en Solresol.");
-                }
-            } catch (Exception e) {
-                System.out.println("Error al verificar la cadena ingresada.");
-            }
-
-            System.out.println("Desea salir? (s/n)");
-            String respuesta;
-            try {
-                respuesta = scanner.nextLine();
-            } catch (Exception e) {
-                System.out.println("Error al leer la entrada del usuario.");
-                continue;
-            }
-            if (respuesta.equalsIgnoreCase("s")) {
-                salir = true;
+            System.out.println("\nSeleccione una opción:");
+            System.out.println("1. Verificar sintaxis de una cadena");
+            System.out.println("2. Obtener antónimo de una palabra");
+            System.out.println("3. Obtener notación abreviada de una palabra");
+            System.out.println("4. Obtener notación completa de una palabra");
+            System.out.println("5. Salir");
+            
+            int opcion = scanner.nextInt();
+            scanner.nextLine(); // Consumir el salto de línea
+            
+            switch (opcion) {
+                case 1:
+                    System.out.println("\nIntroduzca la cadena a verificar:");
+                    String cadena = scanner.nextLine();
+                    
+                    if (solresol.esSolresol(cadena)) {
+                        System.out.println("La cadena es sintácticamente correcta en Solresol.");
+                    } else {
+                        System.out.println("La cadena no es sintácticamente correcta en Solresol.");
+                    }
+                    break;
+                case 2:
+                    System.out.println("\nIntroduzca la palabra de la que quiere obtener su antónimo:");
+                    String palabra = scanner.nextLine();
+                    
+                    String antonimo = solresol.daAntonimo(palabra);
+                    if (antonimo == null) {
+                        System.out.println("La palabra introducida no tiene antónimo en Solresol.");
+                    } else {
+                        System.out.println("El antónimo de " + palabra + " es " + antonimo + ".");
+                    }
+                    break;
+                case 3:
+                    System.out.println("\nIntroduzca la palabra de la que quiere obtener su notación abreviada:");
+                    palabra = scanner.nextLine();
+                    
+                    String notacionAbreviada = solresol.daNotacionAbreviada(palabra);
+                    System.out.println("La notación abreviada de " + palabra + " es " + notacionAbreviada + ".");
+                    break;
+                case 4:
+                    System.out.println("\nIntroduzca la notación abreviada de la palabra:");
+                    String notacionCompleta = scanner.nextLine();
+                    
+                    String palabraCompleta = solresol.daNotacionCompleta(notacionCompleta);
+                    if (palabraCompleta == null) {
+                        System.out.println("No se puede obtener la notación completa de " + notacionCompleta + ".");
+                    } else {
+                        System.out.println("La notación completa de " + notacionCompleta + " es " + palabraCompleta + ".");
+                    }
+                    break;
+                case 5:
+                    salir = true;
+                    System.out.println("¡Gracias por usar el programa de Solresol!");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Introduzca un número del 1 al 5.");
+                    break;
             }
         }
+        
+        scanner.close();
     }
 }
--- Proyectos/Proyecto2/src/Solresol.java
+++ Proyectos/Proyecto2/src/Solresol.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Solresol implements Idioma {
 
     private static final String[] silabas = {"do", "re", "mi", "fa", "sol", "la", "si"};
@@ -74,22 +75,82 @@ public class Solresol implements Idioma {
     }
 
 
-
+    /**
+     * Verifica que una cadena sea sintácticamente correcta en Solresol
+     *
+     * @param cadena La cadena a verificar.
+     * @return true si es válida, false en otro caso.
+     */
     @Override
     public String daAntonimo(String palabra) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'daAntonimo'");
+         
+    }
+    
+
+    public String daNotacionAbreviada(String word) {
+        StringBuilder abreviacion = new StringBuilder();
+        int i = 0;
+        while (i < word.length()) {
+            if (i < word.length() - 2 && word.substring(i, i+2).equals("so")) {
+                // Handle special case for "sol"
+                abreviacion.append("so");
+                i += 3;
+            } else {
+                // Look for the corresponding syllable and add its first letter to the abreviacion
+                String letra = String.valueOf(word.charAt(i)).toLowerCase();
+                for (String silaba : silabas) {
+                    if (silaba.startsWith(letra)) {
+                        abreviacion.append(silaba.substring(0, 1));
+                        break;
+                    }
+                }
+                i++;
+            }
+        }
+        return abreviacion.toString();
     }
 
-    @Override
-    public String daNotacionAbreviada(String palabra) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'daNotacionAbreviada'");
-    }
-
-    @Override
     public String daNotacionCompleta(String abreviacion) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'daNotacionCompleta'");
+        
+
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < abreviacion.length()) {
+            char currentChar = abreviacion.charAt(i);
+            if (currentChar == 's') {
+                // If we encounter an 's', we need to check the next character to see if it's another 's'
+                i++;
+                if (i < abreviacion.length() && abreviacion.charAt(i) == 'o') {
+                    result.append("sol");
+                } else {
+                    result.append("si");
+                    i--;
+                }
+            } else {
+                // For other characters, we can just append their full Solresol notation
+                switch (currentChar) {
+                    case 'd':
+                        result.append("do");
+                        break;
+                    case 'r':
+                        result.append("re");
+                        break;
+                    case 'm':
+                        result.append("mi");
+                        break;
+                    case 'f':
+                        result.append("fa");
+                        break;
+                    case 'l':
+                        result.append("la");
+                        break;
+                }
+            }
+            i++;
+        }
+        return result.toString();
     }
+    
 }
